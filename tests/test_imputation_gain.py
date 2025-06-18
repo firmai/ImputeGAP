023fdefcfb335649e99403354e23b18a7b25ec46
nquen <quentin.nater@gmail.com>
2025-06-18 17:28:37 +0200

    naterq:beta release - llms, validation and dl - docs-readmefile - bf2_2

--- tests/test_imputation_gain.py
+++ tests/test_imputation_gain.py
@@ -29,28 +29,3 @@ class TestGAIN(unittest.TestCase):
         self.assertTrue(abs(metrics["MAE"] - expected_metrics["MAE"]) < 0.2, f"metrics MAE = {metrics['MAE']}, expected MAE = {expected_metrics['MAE']} ")
         self.assertTrue(abs(metrics["MI"] - expected_metrics["MI"]) < 0.4, f"metrics MI = {metrics['MI']}, expected MI = {expected_metrics['MI']} ")
         self.assertTrue(abs(metrics["CORRELATION"] - expected_metrics["CORRELATION"]) < 0.4, f"metrics CORRELATION = {metrics['CORRELATION']}, expected CORRELATION = {expected_metrics['CORRELATION']} ")
-
-
-    def test_imputation_gain_udef(self):
-        """
-        the goal is to test if only the simple imputation with GAIN has the expected outcome
-        """
-        ts_1 = TimeSeries()
-        ts_1.load_series(utils.search_path("eeg-alcohol"))
-        ts_1.normalize(normalizer="min_max")
-
-        incomp_data = ts_1.Contamination.mcar(ts_1.data, rate_series=0.18)
-
-        algo = Imputation.DeepLearning.GAIN(incomp_data).impute(user_def=True, params={"batch_size":32, "hint_rate":0.9, "alpha":10, "epoch":100})
-
-        algo.score(ts_1.data, algo.recov_data)
-        metrics = algo.metrics
-
-        ts_1.print_results(algo.metrics, algo.algorithm)
-
-        expected_metrics = { "RMSE": 0.0792690038670997, "MAE": 0.05888218616139354, "MI": 0.8781148666945359, "CORRELATION": 0.9211668368281689 }
-
-        self.assertTrue(abs(metrics["RMSE"] - expected_metrics["RMSE"]) < 0.2, f"metrics RMSE = {metrics['RMSE']}, expected RMSE = {expected_metrics['RMSE']} ")
-        self.assertTrue(abs(metrics["MAE"] - expected_metrics["MAE"]) < 0.2, f"metrics MAE = {metrics['MAE']}, expected MAE = {expected_metrics['MAE']} ")
-        self.assertTrue(abs(metrics["MI"] - expected_metrics["MI"]) < 0.4, f"metrics MI = {metrics['MI']}, expected MI = {expected_metrics['MI']} ")
-        self.assertTrue(abs(metrics["CORRELATION"] - expected_metrics["CORRELATION"]) < 0.4, f"metrics CORRELATION = {metrics['CORRELATION']}, expected CORRELATION = {expected_metrics['CORRELATION']} ")
